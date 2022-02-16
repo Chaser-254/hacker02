b45ba7af45ffbdcfcebcbc8b846f92f91e67d571
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2022-02-17 00:12:01 +0300

    first commit

---
+++ Addition.py
@@ -0,0 +1,13 @@
+num1 =10
+num2 = 20
+
+sum = num1 + num2
+
+print("sum of {0} and {1} is {2}" .format(num1, num2, sum))
+
+number1 = input("first number: ")
+number2 = input("\nsecond number: ")
+
+sum = float(number1) + float(number2)
+
+print("The sum of {0} and {1} is {2}" .format(number1, number2, sum))
