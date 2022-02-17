3f7e405ddd7a8920fb58b90ee5d60b559d63b301
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2022-02-17 23:45:35 +0300

    python Nested lists

---
+++ PYTHON/Nested for_loop.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+for i in range(1, 5):
+ for j in range(i):
+    print(i, end ='')
+print()
+
+for i in range(1, 5):
+    print(i)
+else:
+    print("No break")
