b48f120c54800655ac34cd9452d76e304f600695
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2022-02-17 23:49:16 +0300

    While loop in python

---
+++ PYTHON/while_loop.py
@@ -0,0 +1,15 @@
+cout = 0
+while (cout < 3):
+    cout = cout + 1
+    print("Hello chaser-254")
+
+else:
+    print("Welcome to programming world!")
+
+cout = 0
+while (cout < 1):
+    cout = cout + 1
+    print(cout)
+    break
+else:
+    print("No Break")
